80e7dd5f5709cda5b5af72460b16ecd444f68065
DevSP <contatodevsp@gmail.com>
2021-04-03 23:37:10 -0300

    Criando a pagina de Posts

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,6 @@
+import Head from 'next/head';
 import { GetStaticProps } from 'next';
+import { FiCalendar, FiUser } from 'react-icons/fi';
 
 import Header from '../components/Header';
 
@@ -29,7 +31,57 @@ interface HomeProps {
 export default function Home(): JSX.Element {
   return (
     <>
+      <Head>
+        <title>Posts | Blog Prismic</title>
+      </Head>
+
       <Header />
+
+      <main className={styles.container}>
+        <a className={styles.contentPost}>
+          <h1 className={commonStyles.title}>Como utilizar Hooks</h1>
+
+          <h2 className={commonStyles.subtitle}>
+            Pensando em sincronização em vez de ciclos de vida.
+          </h2>
+
+          <div className={styles.containerInfo}>
+            <div>
+              <FiCalendar />
+              <span className={commonStyles.info}>15 Mar 2021</span>
+            </div>
+
+            <div>
+              <FiUser />
+              <span className={commonStyles.info}>Joseph Oliveira</span>
+            </div>
+          </div>
+        </a>
+
+        <a className={styles.contentPost}>
+          <h1 className={commonStyles.title}>Como utilizar Hooks</h1>
+
+          <h2 className={commonStyles.subtitle}>
+            Pensando em sincronização em vez de ciclos de vida.
+          </h2>
+
+          <div className={styles.containerInfo}>
+            <div>
+              <FiCalendar />
+              <span className={commonStyles.info}>15 Mar 2021</span>
+            </div>
+
+            <div>
+              <FiUser />
+              <span className={commonStyles.info}>Joseph Oliveira</span>
+            </div>
+          </div>
+        </a>
+      </main>
+
+      <footer className={styles.footer}>
+        <button type="button">Carregar mais posts</button>
+      </footer>
     </>
   );
 }
